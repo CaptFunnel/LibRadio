6a309121807e60ae0e2cd8a4a50e0ed4752f1c7c
Royo Ludovic <lulugimick@hotmail.com>
2025-04-12 09:45:05 +0200

    Initial Commit

---
+++ LibRadio/LibRadioApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibRadioApp.swift
+//  LibRadio
+//
+//  Created by Ludovic royo on 12/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct LibRadioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
